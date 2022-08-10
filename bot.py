6fb1c073ab2948f37c5aade436f44d4ab426cfc1
4khaoz <thaitung.nguyen@yahoo.de>
2022-08-10 22:39:20 +0200

    Refactoring. Can now automatically play next video but there is no sound

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 import vlc
 import random
+import asyncio
 
 load_dotenv()
 
@@ -18,26 +19,27 @@ bot = commands.Bot(
 
 # Variables
 playlist = []
-media_player = vlc.MediaPlayer()
-event_manager = media_player.event_manager()
-
-def __video_finished():
-    pass
+media_player = None
+event_manager = None
+channel = None
 
 
-# Events & Inits
-event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, __video_finished)
-
+#
+# Bot Events
+#
 @bot.event
 async def on_ready():
     print('Logged in')
 
+    global channel
+    channel = bot.get_channel(int(os.getenv('CHANNEL')))
+
     __load_playlist()
-    __play()
+    await playNext()
 
 
 #
-# Commands
+# Bot Commands
 #
 @bot.command()
 async def add(ctx: commands.Context, arg: str):
@@ -68,7 +70,6 @@ async def add(ctx: commands.Context, arg: str):
 
     video_data = {
         "title": info['title'],
-        "source": info['formats'][0]['url'],   # Maybe not needed
         "url": f"https://www.youtube.com/watch?v={info['id']}",
         "thumbnail": info['thumbnail']
     }
@@ -87,7 +88,7 @@ async def play(ctx: commands.Context, title: str = None):
         #media_player.play()
         pass
 
-    __play(__get_random_item())
+    #playNext()
 
 
 @bot.command()
@@ -96,6 +97,15 @@ async def stop(ctx: commands.Context):
     Manual stop command
     """
     pass
+
+
+@bot.command()
+async def volume(ctx: commands.Context, value: int):
+    if value > 100:
+        value = 100
+    if value < 0:
+        value = 0
+    media_player.audio_set_volume(value)
 
 
 def __load_playlist():
@@ -120,20 +130,40 @@ def __save_playlist():
         json.dump(playlist, file, indent=4)
 
 
-def __play(video: dict = None):
+async def playNext(video: dict = None):
+    """
+    Plays the next video
+    @param      video       If video is not given, get a random one
+    """
     if not video:
-        return
+        video = __get_random_item()
 
+    # Send Embed
     embed = discord.Embed(
         title=f"Now playing {video['title']}",
         description=f"{video['url']}"
     )
     embed.set_thumbnail(url=video['thumbnail'])
+    await channel.send(embed=embed)
 
-    #await music_channel.send(embed=embed)
-    #media_player.set_media(video['source'])
-    #media_player.play()
-    vlc.MediaPlayer(video['source']).play()
+    __play(__get_source_from_url(video['url']))
+
+def __play(media: str):
+    """
+    @param      media       Youtube Audio Source URL
+    """
+    global media_player
+    global event_manager
+    media_player = vlc.MediaPlayer(media)
+    event_manager = media_player.event_manager()
+    event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, __video_finished)
+    media_player.play()
+
+
+def __video_finished(event):
+    print(f"Finished {event}")
+    
+    bot.loop.create_task(playNext())
 
 
 def __get_random_item():
@@ -143,6 +173,30 @@ def __get_random_item():
         return random.choice(playlist)
     return None
 
+
+def __get_source_from_url(url: str):
+    """
+    Retrieves the audio source from Youtube URL with yt-dl
+    @param  url     Youtube-URL to video
+    """
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'no_warnings': True,
+    }
+
+    # Extract Youtube Video Data
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(url, download=False)
+        except:
+            raise youtube_dl.utils.DownloadError('RIP')
+
+    return info['formats'][0]['url']
 
 
 # Start bot
