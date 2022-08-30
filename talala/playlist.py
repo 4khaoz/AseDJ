c79c4f4110862e9f05d83780d644f40e0b6f5da1
Senki Gid Gudo <senki.gudo@gmail.com>
2022-08-30 14:58:42 +0200

    refactor: moved music queue implementation to MusicQueue class

--- talala/playlist.py
+++ talala/playlist.py
@@ -1,7 +1,6 @@
 from typing_extensions import Self
 
 import json
-import random
 
 class Playlist:
 
@@ -23,7 +22,6 @@ class Playlist:
     def __init__(self, path: str, items: list[dict] = []) -> None:
         self.path: str           = path
         self.items: list[dict]   = items    # Static List where newly added videos are appended
-        self.__queue: list[dict] = None     # Shuffled list to play
 
 
     def add_item(self, item: dict) -> None:
@@ -32,11 +30,6 @@ class Playlist:
         @param item Video data
         """
         self.items.append(item)
-
-        if self.__queue is None:
-            self.__queue = [item]
-        else:
-            self.__queue.insert(0, item)
 
 
     def item_exists(self, video_id: str) -> bool:
@@ -47,23 +40,9 @@ class Playlist:
         return any(video_id in video_data['id'] for video_data in self.items)
 
 
-    def get_random_item(self) -> dict:
-        """
-        Get random item from playlist
-        """
-        if not self.__queue:
-            self.__load_queue()
-        return self.__queue.pop(0)
-
-
     def save(self) -> None:
         """
         Save playlist-data to JSON-File
         """
         with open(self.path, 'w') as file:
             json.dump(self.items, file, indent=4)
-
-
-    def __load_queue(self) -> None:
-        self.__queue = random.sample(self.items, len(self.items))
-        print("Shuffling queue")
