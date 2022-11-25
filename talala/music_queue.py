f5fb9baefac5cd5e0c265f0e50a6e454aa591d31
Senki Gid Gudo <senki.gudo@gmail.com>
2022-11-25 09:18:52 +0100

    improved shuffle so that songs don't occur more than once in the same queue

--- talala/music_queue.py
+++ talala/music_queue.py
@@ -1,5 +1,3 @@
-import random
-
 import asyncio
 
 from typing import Optional
@@ -9,6 +7,7 @@ from datetime import datetime
 from talala import yt_utils
 from talala.playlist import Playlist
 from talala.playlist import Video
+from talala.shuffle import weighted_shuffle
 
 
 class MusicQueue:
@@ -24,6 +23,25 @@ class MusicQueue:
         self.next_source: Optional[str] = None
 
         self.__queue: Optional[list[Video]] = None     # Shuffled list to play
+
+    @property
+    def playlist_items(self) -> list[Video]:
+        return self.playlist.items
+
+    @property
+    def playlist_item_weights(self) -> list[float]:
+        now = datetime.now()
+
+        unmapped_weights: list[int] = [(now - item.last_played_at).total_seconds() if item.last_played_at is not None else 0 for item in self.playlist_items]
+
+        old_max = max(unmapped_weights)
+        old_min = min(unmapped_weights)
+        old_range = old_max - old_min
+        new_min = 0.1
+        new_max = 1.0
+        new_range = new_max - new_min
+
+        return [(((old_weight - old_min) * new_range) / old_range + new_min) if old_range > 0 else 0.1 for old_weight in unmapped_weights]
 
     def enqueue_item(self, video: Video) -> None:
         if self.__queue is None:
@@ -60,8 +78,5 @@ class MusicQueue:
         self.next_source = yt_utils.get_source_from_url(self.next_video.url)
 
     def __load_queue(self) -> None:
-
-        items: list[Video] = self.playlist.items
-
-        self.__queue = random.choices(items, k=len(self.playlist.items), weights=self.playlist.item_weights())
         print("Shuffling queue")
+        self.__queue = weighted_shuffle(self.playlist_items, self.playlist_item_weights)
